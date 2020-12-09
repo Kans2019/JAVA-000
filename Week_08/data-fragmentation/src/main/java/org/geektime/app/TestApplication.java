1f8ff41d575fee6f8292566c3955b66e88661296
Terrdi <675464934@qq.com>
2020-12-10 00:38:50 +0800

    shardingsphere-jdbc版本提交

--- Week_08/data-fragmentation/src/main/java/org/geektime/app/TestApplication.java
+++ Week_08/data-fragmentation/src/main/java/org/geektime/app/TestApplication.java
@@ -1,5 +1,6 @@
 package org.geektime.app;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.geektime.pojo.Order;
 import org.geektime.service.IOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +24,27 @@ public class TestApplication implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        orderService.remove(new QueryWrapper<>());
+//        try {
+//            Order order = new Order();
+//            order.setId(1);
+//            order.setUserId(-1234231);
+//            order.setOrderNo(112312312L);
+//            orderService.save(order);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//        try {
+//            Order order = new Order();
+//            order.setId(1);
+//            order.setUserId(1234231);
+//            order.setOrderNo(-112312312L);
+//            orderService.save(order);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
         for (int i = 0; i < 10; i++) {
             orderService.save(generate());
         }
@@ -32,7 +54,7 @@ public class TestApplication implements ApplicationRunner {
 
     protected Order generate() {
         Random random = new Random();
-        return new Order(random.nextInt(3000), random.nextLong(), random.nextInt(),
+        return new Order(random.nextInt(3000), Math.abs(random.nextLong()), Math.abs(random.nextInt()),
                 random.nextInt(), BigDecimal.valueOf(random.nextLong() & 0xFFFFFF), random.nextInt(4),
                 random.nextInt(10), random.nextInt(4), new Date(), new Date(), new Date(), new Date(), new Date(), new Date());
     }
--- Week_08/data-fragmentation/src/main/java/org/geektime/conf/ShardingSphereConfiguration.java
+++ Week_08/data-fragmentation/src/main/java/org/geektime/conf/ShardingSphereConfiguration.java
@@ -1,31 +1,19 @@
-package org.geektime.data.source;
+package org.geektime.conf;
 
 import com.baomidou.mybatisplus.core.MybatisConfiguration;
 import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.type.JdbcType;
-import org.apache.shardingsphere.infra.spi.ShardingSphereServiceLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor;
-import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 
-import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 
-/**
- * @author <a href="mailto:675464934@qq.com">Terrdi</a>
- * @date 2020/12/4
- * @since 1.8
- **/
-@Profile({"shardingsphere-jdbc", "shardingsphere-proxy"})
 @Configuration
 public class ShardingSphereConfiguration {
     private static final Logger logger = LoggerFactory.getLogger(ShardingSphereConfiguration.class);
